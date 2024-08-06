30687ce126067b362674965bbbce0fc1c79b4a15
Toke Camps <toke@dataenhancing.com>
2024-08-06 11:32:47 +0200

    main function

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from drive_connection import authenticate, create_creds
 from create_vectorstore import run_vs
 
-def sidebar():
+
+def main():
+    # Run the sidebar 
     if 'authcomplete' not in st.session_state:
         st.session_state.authcomplete = False
     if 'auth_url' not in st.session_state:
@@ -38,10 +40,6 @@ def sidebar():
     if st.session_state.authcomplete:
         if st.sidebar.button('Update Vectorstore'):
             run_vs()
-
-def main():
-    # Run the sidebar 
-    sidebar()
 
 
             
